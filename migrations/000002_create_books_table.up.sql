2273b04741976e55e4f5e17d08eefb005b704ff5
0xrinful <0xrinful@gmail.com>
2025-12-09 16:45:17 +0200

    add migrations

---
+++ migrations/000002_create_books_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS books (
+  id bigserial PRIMARY KEY,
+  title text NOT NULL,
+  author text NOT NULL,
+  publish_date DATE NOT NULL,
+  isbn VARCHAR(17) UNIQUE NOT NULL,
+  description text NOT NULL,
+  copies_total integer NOT NULL DEFAULT 1,
+  copies_available integer NOT NULL DEFAULT 1,
+  version integer NOT NULL DEFAULT 1
+);
---
+++ migrations/000003_create_borrow_records_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS borrow_records (
+  id bigserial PRIMARY KEY,
+  user_id bigint NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+  book_id bigint NOT NULL REFERENCES books (id) ON DELETE CASCADE,
+  borrowed_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  returned_at timestamp(0) with time zone NULL,
+);
