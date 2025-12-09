c9c028ed8599c6f8858ba4b4d98d4b13a7d643e1
0xrinful <0xrinful@gmail.com>
2025-12-09 18:44:46 +0200

    add graceful shutdown

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,89 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+
+	"github.com/0xrinful/LibraryMS/internal/logger"
+)
+
+type config struct {
+	port int
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
+}
+
+type application struct {
+	config config
+	logger *logger.Logger
+}
 
 func main() {
-	fmt.Println("Start")
+	cfg := parseFlags()
+	logger := logger.New(os.Stdout, logger.LevelInfo)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.PrintFatal(err)
+	}
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err)
+	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func parseFlags() config {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 8000, "Web Server port")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(
+		&cfg.db.maxIdleTime,
+		"db-max-idle-time",
+		"15m",
+		"PostgreSQL max connection idle time",
+	)
+	flag.Parse()
+	return cfg
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/0xrinful/rush"
+)
+
+func (app *application) routes() http.Handler {
+	r := rush.New()
+
+	return r
+}
